bdeb9c855e07f95217e34854da17d386a5d02d0b
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-06-08 23:37:11 +0200

    Function for partitionwise minimization

---
+++ R/partwise_minimize.R
@@ -0,0 +1,263 @@
+
+#' Title
+#'
+#' @param x  
+#' @param units Units defining the within-dimension of data (time series)
+#' @param time Periods defining the between-dimension of data (cross sections)
+#' @param cond Conditions used for the pooled analysis
+#' @param out Outcome used for the pooled analysis
+#' @param n_cut Frequency cut-off for designating truth table rows as observed
+#' @param incl_cut Inclusion cut-off for designating truth table rows as
+#' consistent
+#' @param solution 
+#' @param BE_cons 
+#' @param WI_cons 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+partwise_minimize <- function(x, units, time, cond, out, n_cut, incl_cut, solution, BE_cons, WI_cons) {
+  
+  # Turning of warnings
+  # options(warn = -1)
+  
+  # Splitting the data if time and unit values are available
+  if (missing(units)) {
+    colnames(x)[which(names(x) == time)] <- "time"
+    x <- x[with(x, order(time)), ]
+    xB <- x
+    xB$consis <- rep(incl_cut, times = nrow(x))
+    xxx <- 1
+    BE_list <- split(xB, xB[, "time"])
+  } else if (missing(time)) {
+    colnames(x)[which(names(x) == units)] <- "units"
+    x <- x[with(x, order(units)), ]
+    xW <- x
+    xW$consis <- rep(incl_cut, times = nrow(x))
+    xxx <- 2
+    WI_list <- split(xW, xW[, "units"])
+    
+  } else {
+    xxx <- 3
+    colnames(x)[which(names(x) == time)] <- "time"
+    colnames(x)[which(names(x) == units)] <- "units"
+    
+    x <- x[with(x, order(time)), ]
+    
+    lengtht <- length(unique(x$time))
+    lengthu <- length(unique(x$units))
+    
+    xB <- x
+    xW <- x
+    
+    # Assigning individual consistency thresholds if available
+    if (missing(BE_cons)) {
+      BE_cons <- rep(incl_cut, times = lengtht)
+      xB$consis <- rep(incl_cut, times = nrow(x))
+    } else {
+      BE_cons <- BE_cons
+      xB$consis <- rep(BE_cons, each = lengthu)
+    }
+    if (missing(WI_cons)) {
+      WI_cons <- rep(incl_cut, times = lengthu)
+      xW$consis <- rep(incl_cut, times = nrow(x))
+    } else {
+      WI_cons <- WI_cons
+      xW$consis <- rep(WI_cons, times = lengtht)
+    }
+    BE_list <- split(xB, xB[, "time"])
+    WI_list <- split(xW, xW[, "units"])
+  }
+  
+  x$consis <- incl_cut
+  PO_list <- list(x)
+  
+  # Creating a function for the transformation of QCA results
+  paster <- function(x) {
+    x <- paste(x, collapse = "+")
+    x
+  }
+  
+  #### Function for Between and Within Solutions ####
+  pqmcc <- function(x) {
+    
+    if (xxx == 1) {
+      part <- as.character(x$time[1])
+      type <- "between"
+    } else if (xxx == 2) {
+      part <- as.character(x$units[1])
+      type <- "within"
+    } else {
+      partition <- unlist(x$time)
+      if (partition[1] == partition[2]) {
+        part <- as.character(x$time[1])
+        type <- "between"
+      } else {
+        part <- as.character(x$units[1])
+        type <- "within"
+      }
+    }
+    
+    check <- x[cond]
+    check[check < 0.5] <- 0
+    check[check > 0.5] <- 1
+    check2 <- as.data.frame(colMeans(check))
+    check2[check2 == 1] <- 0
+    check3 <- as.numeric(colMeans(check2))
+    
+    if (check3 == 0) {
+      x$consistency <- "-"
+      zz <- as.data.frame(x$consistency)
+      zz$coverage <- "-"
+      zz$solution <- "No variation in all coniditions"
+      zz$model <- "-"
+      zz$partition <- part
+      zz$type <- type
+      zz <- zz[!duplicated(zz), ]
+      colnames(zz)[1] <- "consistency"
+    } else {
+      
+      # s <- has_error(truthTable(x, outcome = out, conditions = cond, incl.cut1 = x[,ncol(x)][1], n.cut = n_cut))
+      s <- testit::has_error(susu <- try(QCA::truthTable(x, outcome = out, conditions = cond, incl.cut1 = x[, ncol(x)][1], n.cut = n_cut), silent = TRUE))
+      
+      if (s == F) {
+        x1 <- try(QCA::truthTable(x, outcome = out, conditions = cond, incl.cut1 = x[, ncol(x)][1], n.cut = n_cut), silent = TRUE)
+        
+        x2 <- x1$tt$OUT
+        x2[x2 == "?"] <- NA
+        x2 <- as.numeric(x2)
+        # x2[is.na(x2)] <- 0.5
+        x2 <- na.omit(x2)
+        x2 <- mean(x2)
+        
+        if (x2 == 0) {
+          
+          x$consistency <- "-"
+          zz <- as.data.frame(x$consistency)
+          zz$coverage <- "-"
+          zz$solution <- "All inconsistent"
+          zz$model <- "-"
+          zz$partition <- part
+          zz$type <- type
+          zz <- zz[!duplicated(zz), ]
+          colnames(zz)[1] <- "consistency"
+          
+        } else if (x2 == 1 & solution == "P") {
+          
+          x$consistency <- "-"
+          zz <- as.data.frame(x$consistency)
+          zz$coverage <- "-"
+          zz$solution <- "All consistent"
+          zz$model <- "-"
+          zz$partition <- part
+          zz$type <- type
+          zz <- zz[!duplicated(zz), ]
+          colnames(zz)[1] <- "consistency"
+          
+        } else {
+          
+          if (solution == "C") {
+            x <- QCA::minimize(x1, explain = "1", include = "1", details = T, show.cases = T, all.sol = T, row.dom = F)
+          } else if (solution == "P") {
+            x <- QCA::minimize(x1, explain = "1", include = "?", details = T, show.cases = T, all.sol = T, row.dom = F)
+          } else {
+            x <- QCA::minimize(x1, explain = "1", include = "1", details = T, show.cases = T, all.sol = T, row.dom = F)
+          }
+          
+          # x$CONS <- x$IC$incl.cov$incl[1]
+          x$consistency <- x$IC$sol.incl.cov[1]
+          
+          if (is.null(x$consistency)) {
+            BSP2 <- x$IC
+            BSP3 <- BSP2$individual
+            neu <- sapply(BSP3, function(x) x[2])
+            x$consistency <- sapply(neu, function(x) x[1])
+            x$consistency <- unlist(x$consistency)
+          }
+          
+          zz <- as.data.frame(x$consistency)
+          x$coverage <- x$IC$sol.incl.cov[3]
+          
+          if (is.null(x$coverage)) {
+            BSP2 <- x$IC
+            BSP3 <- BSP2$individual
+            neu <- sapply(BSP3, function(x) x[2])
+            x$coverage <- sapply(neu, function(x) x[3])
+            x$coverage <- unlist(x$coverage)
+          }
+          
+          zz$coverage <- as.numeric(x$coverage)
+          x$solution <- x$solution[]
+          tete <- list(cons = x$solution[])
+          neux <- lapply(tete$cons, paster)
+          neuxx <- unlist(neux)
+          zz$solution <- neuxx
+          numberrows <- nrow(zz)
+          if (numberrows == 1) {
+            zz$model <- 1
+          } else {
+            zz$model <- as.numeric(rownames(zz))
+          }
+          zz$partition <- part
+          zz$type <- type
+          colnames(zz)[1] <- "consistency"
+        }
+        
+      } else {
+        x$consistency <- "-"
+        zz <- as.data.frame(x$consistency)
+        zz$coverage <- "-"
+        zz$solution <- "no combinations at this frequency cutoff"
+        zz$model <- "-"
+        zz$partition <- part
+        zz$type <- type
+        zz <- zz[!duplicated(zz), ]
+        colnames(zz)[1] <- "consistency"
+      }
+    }
+    zz
+  }
+  
+  
+  #### Application of Function ####
+  
+  if (missing(time)) {
+    WI_list1 <- lapply(WI_list, pqmcc)
+    PO_list1 <- lapply(PO_list, pqmcc)
+    dff2 <- plyr::ldply(WI_list1)[, -1]
+    dff3 <- plyr::ldply(PO_list1)[, ]
+    dff3$type <- "pooled"
+    dff3$partition <- "-"
+    total <- rbind(dff3, dff2)
+    
+  } else if (missing(units)) {
+    BE_list1 <- lapply(BE_list, pqmcc)
+    PO_list1 <- lapply(PO_list, pqmcc)
+    dff1 <- plyr::ldply(BE_list1)[, -1]
+    dff3 <- plyr::ldply(PO_list1)[, ]
+    dff3$type <- "pooled"
+    dff3$partition <- "-"
+    total <- rbind(dff3, dff1)
+    
+  } else {
+    BE_list1 <- lapply(BE_list, pqmcc)
+    WI_list1 <- lapply(WI_list, pqmcc)
+    PO_list1 <- lapply(PO_list, pqmcc)
+    
+    dff1 <- plyr::ldply(BE_list1)[, -1]
+    dff2 <- plyr::ldply(WI_list1)[, -1]
+    dff3 <- plyr::ldply(PO_list1)[, ]
+    dff3$type <- "pooled"
+    dff3$partition <- "-"
+    total <- rbind(dff3, dff1, dff2)
+  }
+  
+  #### Rounding ####
+  total$consistency[total$model == "-"] <- NA
+  total$coverage[total$model == "-"] <- NA
+  total$consistency <- as.numeric(total$consistency)
+  total$coverage <- as.numeric(total$coverage)
+  
+  return(total)
+}
