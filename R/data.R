9bfa01380fcddb5e32ec63e7482d54e7be7cc37a
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-06-17 23:19:35 +0200

    Add description for Thiem (2011)

--- R/data.R
+++ R/data.R
@@ -13,3 +13,20 @@
 #' }
 #' @source \url{https://doi.org/10.1080/07036337.2016.1203309}
 "Schwarz2016"
+
+#' Original data used by Thiem (201)
+#'
+#' A dataset containing the calibrated set values for the article:
+#' Thiem, Alrik (2011): 
+#' Conditions of Intergovernmental Armaments Cooperation in Western Europe,
+#'  1996-2006. European Political Science Review 3 (1): 1-33.
+#'
+#' @format A data frame with 165 rows and 10 variables:
+#' \describe{
+#'   \item{id}{Country-year ID}
+#'   \item{time}{time ID}
+#'   ...
+#' }
+#' @source \url{https://doi.org/10.1017/S1755773910000251}
+"Thiem2011"
+
