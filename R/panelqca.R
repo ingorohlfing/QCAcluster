771dd82dcd873b32e199a61921788f14d05a7c95
ingorohlfing <i.rohlfing@uni-koeln.de>
2020-07-07 10:33:15 +0200

    Clean script

--- R/panelqca.R
+++ R/panelqca.R
@@ -1,13 +1,4 @@
 
-
-library(QCA)
-library(data.table)
-library(plyr)
-library(testit)
-library(devtools)
-library(testthat)
-has_devel()
-
 ### Schwarz 2016
 
 
@@ -33,17 +24,3 @@ grauvogel_WOP <- WOPfunctionInter(grauvogel2014, units = "Sender", cond = c("Com
 
 ### Schneider 2014
 
-schneider2014 <- read.csv("schneider2014.csv", as.is = T, sep = ";")
-
-schneider2014$year <- schneider2014$X
-schneider2014$year <- gsub("[A-Z]", "", schneider2014$year)
-unique(schneider2014$year)
-
-schneider2014_panelp <- PWBfunctionCP(schneider2014, time = "year", cond = c("high_lmx_l2", "high_wc_l2", "high_ud_l2", "high_epl_l2"), 
-    out = "low_fde_l2", n_cut = 1, incl_cut = 0.8, solution = "P")
-
-schneider2014_DIV <- DIVfunction(schneider2014, time = "year", cond = c("high_lmx_l2", "high_wc_l2", "high_ud_l2", "high_epl_l2"), out = "low_fde_l2", 
-    n_cut = 1, incl_cut = 0.8)
-
-schneider2014_WOP <- WOPfunctionCP(schneider2014, time = "year", cond = c("high_lmx_l2", "high_wc_l2", "high_ud_l2", "high_epl_l2"), 
-    out = "low_fde_l2", n_cut = 9, incl_cut = 0.8, solution = "P")
