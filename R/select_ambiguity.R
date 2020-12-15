1ae501548505f2b2fd78452ca0e21efaaab1c110
AyjerenR <jeren.jmv@gmail.com>
2020-12-15 13:44:46 +0100

    updating the select function

--- R/select_ambiguity.R
+++ R/select_ambiguity.R
@@ -5,12 +5,20 @@
 #'
 #'
 #' @param sol QCA solution
-#' @param elmnt is specific part of the solution that 
+#' @param elmnt is specific part of the solution that
+#' @param overall a logical argument that allows to move between different 
+#' levels of ambiguous solution. When FALSE returns elements per each model
+#' within an ambiguous solution. When TRUE returns overall values
 #' is needed (as saved by QCA package)
 #' @return returns coverage and consistency information
 #' for each part of the ambiguous solution
 
-select.fnc <- function(sol, elmnt){
-  tmp <- map(sol$IC$individual, function(x) x[[elmnt]])
+select.fnc <- function(sol, elmnt, overall = FALSE){
+  
+  if (overall == TRUE) {
+    tmp <- sol$IC[[elmnt]]
+  } else {
+    tmp <- map(sol$IC$individual, function(x) x[[elmnt]])
+  }
   return(tmp)
 }
