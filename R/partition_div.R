842de001a5cfb3cb287d226c5cdf72584d41bff7
ingorohlfing <i.rohlfing@uni-koeln.de>
2021-05-09 09:54:30 +0200

    Update documentation

--- R/partition_div.R
+++ R/partition_div.R
@@ -10,7 +10,8 @@
 #' @importFrom testit has_error
 #' @import QCA
 #' 
-#' @param x Calibrated pooled dataset for partitioning and minimization
+#' @param dataset Calibrated pooled dataset that is partitioned and minimized for
+#' deriving the pooled solution.
 #' @param units Units defining the within-dimension of data (time series)
 #' @param time Periods defining the between-dimension of data (cross sections)
 #' @param cond Conditions used for the pooled analysis
@@ -18,28 +19,6 @@
 #' @param n_cut Frequency cut-off for designating truth table rows as observed
 #' @param incl_cut Inclusion cut-off for designating truth table rows as
 #' consistent
-#' @param BE_cons Inclusion (or consistency) thresholds for cross sections. 
-#' Must be specified as a numeric vector with length equaling the number of
-#' cross sections. Numbers correspond to the order of the cross section ID
-#' in the data (such as years in ascending order).
-#' @param WI_cons Inclusion (or consistency) thresholds for time series. 
-#' Must be specified as a numeric vector with length equaling the number of
-#' time series. Numbers correspond to the order of the time series (unit) ID
-#' in the data (such as countries in alphabetical order).
-#' @param BE_ncut For *cross sections*, the minimum number of members needed
-#' for declaring a truth table row 
-#' as relevant as opposed to designating it as a remainder.
-#' Must be specified as a numeric vector. Its length should be
-#' equal the number of cross sections. The order of thresholds corresponds
-#' to the order of the cross sections in the data defined by the cross-section
-#' ID in the dataset (such as years in ascending order).
-#' @param WI_ncut For *time series*, the minimum number of members needed
-#' for declaring a truth table row 
-#' as relevant as opposed to designating it as a remainder. 
-#' Must be specified as a numeric vector. Its length should be
-#' equal the number of time series. The order of thresholds corresponds
-#' to the order of the of the time-series (unit) ID
-#' in the dataset (such as countries in alphabetical order).
 #' 
 #' @return A dataframe presenting the diversity of cases belonging to the
 #' same partition with the following columns:
@@ -75,9 +54,7 @@
 diversity <- function(dataset, 
                       units, time, 
                       cond, out, 
-                      n_cut, incl_cut, 
-                      BE_cons, WI_cons, 
-                      BE_ncut, WI_ncut) {
+                      n_cut, incl_cut) {
   
   # turning of warnings
   quiet <- function(x) { 
@@ -86,73 +63,12 @@ diversity <- function(dataset,
     invisible(force(x)) 
   } 
   
-  x1 <- dataset
-  x2 <- dataset
-  if (!missing(BE_cons) & !missing(time)) {
-    colnames(x1)[which(names(x1) == time)] <- "time"
-    beconstest <- all(length(unique(x1$time)) == length(BE_cons))  
-  } else{
-    beconstest <- T
-  } 
-  if (!missing(WI_cons) & !missing(units)) {
-    colnames(x1)[which(names(x1) == units)] <- "units"
-    wiconstest <- all(length(unique(x1$units)) == length(WI_cons))  
-  } else{
-    wiconstest <- T
-  } 
-  if (!missing(BE_ncut) & !missing(time)) {
-    colnames(x2)[which(names(x2) == time)] <- "time"
-    bencuttest <- all(length(unique(x2$time)) == length(BE_ncut))  
-  } else{
-    bencuttest <- T
-  } 
-  if (!missing(WI_ncut) & !missing(units)) {
-    colnames(x2)[which(names(x2) == units)] <- "units"
-    wincuttest <- all(length(unique(x2$units)) == length(WI_ncut))  
-  } else{
-    wincuttest <- T
-  } 
-  
-  if(beconstest == F) {
-    stop("The number of BE_cons values does not match the number of periods")
-  } else {
-    if(wiconstest == F) {
-      stop("The number of WI_cons values does not match the number of units")
-    } else {
-      if(bencuttest == F) {
-        stop("The number of BE_ncut values does not match the number of periods")
-      } else {  
-        if(wincuttest == F) {
-          stop("The number of WI_ncut values does not match the number of units")
-        } else {
-          
-  
   # splitting the data if time and unit values are available
   x <- dataset
   if (missing(units)) {
     colnames(x)[which(names(x) == time)] <- "time"
     xB <- x
     
-    if (missing(BE_cons)) {
-      xB$consis <- rep(incl_cut, times = nrow(x))
-    } else {
-      
-      time_data <- as.data.frame(unique(x$time))
-      names(time_data)[1] <- "time"
-      time_data$consis <- BE_cons
-      
-      xB <- merge(xB, time_data, by = "time", all.x = T)
-    }
-    if (missing(BE_ncut)) {
-      xB$ncut <- rep(n_cut, times = nrow(x))
-    } else {
-      
-      time_data1 <- as.data.frame(unique(x$time))
-      names(time_data1)[1] <- "time"
-      time_data1$ncut <- BE_ncut
-      
-      xB <- merge(xB, time_data1, by = "time", all.x = T)
-    }
     xxx <- 1
     BE_list <- split(xB, xB[, "time"])
     
@@ -161,26 +77,6 @@ diversity <- function(dataset,
     colnames(x)[which(names(x) == units)] <- "units"
     xW <- x
     
-    if (missing(WI_cons)) {
-      xW$consis <- rep(incl_cut, times = nrow(x))
-    } else {
-      
-      unit_data <- as.data.frame(unique(x$units))
-      names(unit_data)[1] <- "units"
-      unit_data$consis <- WI_cons
-      
-      xW <- merge(xW, unit_data, by = "units", all.x = T)
-    }
-    if (missing(WI_ncut)) {
-      xW$ncut <- rep(n_cut, times = nrow(x))
-    } else {
-      
-      unit_data1 <- as.data.frame(unique(x$units))
-      names(unit_data1)[1] <- "units"
-      unit_data1$ncut <- WI_ncut
-      
-      xW <- merge(xW, unit_data1, by = "units", all.x = T)
-    }
     xxx <- 2
     WI_list <- split(xW, xW[, "units"])
     
@@ -192,66 +88,16 @@ diversity <- function(dataset,
     xB <- x
     xW <- x
     
-    # Assigning individual consistency thresholds if available
-    if (missing(BE_cons)) {
-      xB$consis <- rep(incl_cut, times = nrow(x))
-    } else {
-      
-      time_data <- as.data.frame(unique(x$time))
-      names(time_data)[1] <- "time"
-      time_data$consis <- BE_cons
-      
-      xB <- merge(xB, time_data, by = "time", all.x = T)
-    }
-    if (missing(BE_ncut)) {
-      xB$ncut <- rep(n_cut, times = nrow(x))
-    } else {
-      
-      time_data1 <- as.data.frame(unique(x$time))
-      names(time_data1)[1] <- "time"
-      time_data1$ncut <- BE_ncut
-      
-      xB <- merge(xB, time_data1, by = "time", all.x = T)
-    }
-    
-    if (missing(WI_cons)) {
-      xW$consis <- rep(incl_cut, times = nrow(x))
-    } else {
-      
-      unit_data <- as.data.frame(unique(x$units))
-      names(unit_data)[1] <- "units"
-      unit_data$consis <- WI_cons
-      
-      xW <- merge(xW, unit_data, by = "units", all.x = T)
-    }
-    if (missing(WI_ncut)) {
-      xW$ncut <- rep(n_cut, times = nrow(x))
-    } else {
-      
-      unit_data1 <- as.data.frame(unique(x$units))
-      names(unit_data1)[1] <- "units"
-      unit_data1$ncut <- WI_ncut
-      
-      xW <- merge(xW, unit_data1, by = "units", all.x = T)
-    }
-    
     BE_list <- split(xB, xB[, "time"])
     WI_list <- split(xW, xW[, "units"])
   }
   
-  x$consis <- incl_cut
-  x$ncut <- n_cut
   PO_list <- list(x)
   
   paster <- function(x) {
     x <- paste(x, collapse = "+")
     x
   }
-  #x <- BE_list[1]
-  #x <- as.data.frame(BE_list[1])
-  #x <- Thiem2011[Thiem2011$time == 1996,]
-  #x$consis <- 0.8
-  #x$ncut <- 2
   
   #### Function for Between and Within Solutions ####
   pqmcc <- function(x) {
@@ -286,16 +132,18 @@ diversity <- function(dataset,
       zz$type <- type
       zz$diversity <- "No variation in all coniditions"
       zz$diversity_per <- "-"
+      zz$diversity_1 <- "-"
+      zz$diversity_0 <- "-"
       zz <- zz[!duplicated(zz), ]
       colnames(zz)[1] <- "partition"
       
     } else {
       
       # s <- has_error(truthTable(x, outcome = out, conditions = cond, incl.cut1 = x[,ncol(x)][1], n.cut = n_cut))
-      s <- testit::has_error(susu <- try(suppressWarnings(QCA::truthTable(x, outcome = out, conditions = cond, incl.cut1 = x[, ncol(x)-1][1], n.cut = x[, ncol(x)][1])), silent = TRUE))
+      s <- testit::has_error(susu <- try(suppressWarnings(QCA::truthTable(x, outcome = out, conditions = cond, incl.cut1 = incl_cut, n.cut = n_cut)), silent = TRUE))
       
       if (s == F) {
-        x1 <- try(suppressWarnings(QCA::truthTable(x, outcome = out, conditions = cond, incl.cut1 = x[, ncol(x)-1][1], n.cut = x[, ncol(x)][1])), silent = TRUE)
+        x1 <- try(suppressWarnings(QCA::truthTable(x, outcome = out, conditions = cond, incl.cut1 = incl_cut, n.cut = n_cut)), silent = TRUE)
         
         zz <- as.data.frame(part)
         zz$type <- type
@@ -312,10 +160,11 @@ diversity <- function(dataset,
         zz$type <- type
         zz$diversity <- "no combinations at this frequency cutoff"
         zz$diversity_per <- "-"
+        zz$diversity_1 <- "-"
+        zz$diversity_0 <- "-"
         zz <- zz[!duplicated(zz), ]
         colnames(zz)[1] <- "partition"
       }
-      
     }
     zz
   }
@@ -365,13 +214,24 @@ diversity <- function(dataset,
   total$diversity_1 <- as.numeric(total$diversity_1)
   total$diversity_0 <- as.numeric(total$diversity_0)
   
-  y <- as.numeric(max(total$diversity, na.rm = T))
+  if(length(unique(total$diversity)) == 1){
+    total$diversity_per <- "-"
+    total$diversity_per_1 <- "-"
+    total$diversity_per_0 <- "-"
+    total$diversity <- "-"
+    total$diversity <- total$diversity_old
+    total$diversity_old <- NULL
+  }else{
+    
+    y <- as.numeric(max(total$diversity, na.rm = T))
+    
+    total$diversity_per <- ifelse(is.na(total$diversity), NA, total$diversity/y)
+    total$diversity_per_1 <- ifelse(is.na(total$diversity_1), NA, total$diversity_1/y)
+    total$diversity_per_0 <- ifelse(is.na(total$diversity_0), NA, total$diversity_0/y)
+    total$diversity <- total$diversity_old
+    total$diversity_old <- NULL}
   
-  total$diversity_per <- ifelse(is.na(total$diversity), NA, total$diversity/y)
-  total$diversity_per_1 <- ifelse(is.na(total$diversity_1), NA, total$diversity_1/y)
-  total$diversity_per_0 <- ifelse(is.na(total$diversity_0), NA, total$diversity_0/y)
-  total$diversity <- total$diversity_old
-  total$diversity_old <- NULL
+  
   total <- total[, c(2, 1, 3, 4, 5, 6, 7, 8)]
-  return(total)}}}}
-}
+  
+  return(total)}
