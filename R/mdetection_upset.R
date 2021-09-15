8dae0b93eba30f6d268bce7d776f627a06bcd993
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2021-09-15 21:10:49 +0200

    Update polished function and documenation

--- R/mdetection_upset.R
+++ R/mdetection_upset.R
@@ -1,13 +1,12 @@
-#' Internal comparison function for configurations.
+#' Internal comparison function for aggregation over configurations.
 #'
 #' @importFrom  stringi stri_detect_fixed
 #'
-#' @param ls List of QCA solutions 
+#' @param ls List of QCA models 
 #' @noRd
 #'
-#' @return A list counting the individual solutions
-#' or configurations.
-mdetection_upset <- function(ls, x){
+#' @return A list counting the individual models or configurations.
+mdetection_upset <- function(ls, x) {
   mtr <- NULL
   for(l in x) {
     vctr <- as.numeric(stringi::stri_detect_fixed(ls, l))
