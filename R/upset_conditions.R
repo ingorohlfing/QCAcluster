a37a58a51ff255eed255575103d7287503dc52d6
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2021-09-15 14:09:13 +0200

    Add check and error message for missing solution column

--- R/upset_conditions.R
+++ R/upset_conditions.R
@@ -42,9 +42,14 @@ upset_conditions <- function(df, nsets) {
   if (!is.data.frame(df)) {
     stop('Models should be part of a dataframe or tibble. 
     Object used in function is not a dataframe or tibble.') 
-    }
+  }
+  if ("solution" %in% colnames(df) == FALSE) {
+    stop('There is no column named "solution" in the object.
+         The models to be aggregated over should be in a column "solution".')
+  }
   else {
-  temp1 <- purrr::map(unlist(df$solution), function(x)stringi::stri_split_fixed(x, "*") %>% 
+  temp1 <- purrr::map(unlist(df$solution), 
+                      function(x)stringi::stri_split_fixed(x, "*") %>% 
                         unlist())
   temp1 <- purrr::map(temp1, function(x)
     stringi::stri_split_fixed(x, "+") %>% unlist())
